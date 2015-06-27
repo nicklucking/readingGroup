2b1234593babf1bd3ea67dbbaef2b44244bc0366
Nick Lucking <lucking.nick@bcgdv.com>
2015-06-27 12:04:29 -0700

    inital checkin

---
+++ Podfile
@@ -0,0 +1,19 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '6.0'
+
+
+
+pod 'Fabric', '~> 1.3.0' # required
+pod 'Fabric/Crashlytics', '~> 1.3.0'
+pod 'Spring', '~> 1.0.3'
+
+use_frameworks!
+
+target 'Reading Group' do
+
+end
+
+target 'Reading GroupTests' do
+
+end
+
---
+++ Reading GroupTests/Reading_GroupTests.swift
@@ -0,0 +1,36 @@
+//
+//  Reading_GroupTests.swift
+//  Reading GroupTests
+//
+//  Created by Nicholas Lucking on 6/27/15.
+//  Copyright (c) 2015 Nicholas Lucking. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Reading_GroupTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
